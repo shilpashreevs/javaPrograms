a03626133c9027a5d9ee2e8d6388fd94ecc5851c
shilpashreevs <48344640+shilpashreevs@users.noreply.github.com>
2019-03-21 17:25:31 +0530

    Add files via upload

---
+++ revary.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class ArrayReversalDemo 
+{
+ public static void main(String[] args)
+ {
+ int[] numbers = {1, 2, 3, 4, 5, 6, 7}; 
+reverse(numbers); 
+}
+public static void reverse(int[] input)
+ { 
+System.out.println("original array : " + Arrays.toString(input));
+
+if(input == null || input.length <= 1)
+{ 
+return;
+ } 
+
+for (int i = 0; i < input.length / 2; i++) 
+{ int temp = input[i];
+ // swap numbers input[i] = input[input.length - 1 - i]; 
+input[input.length - 1 - i] = temp; 
+}
+ System.out.println("reversed array : " + Arrays.toString(input)); 
+} 
+}
+
